73f89df4f3040a26cc6e9e32d39e5cdd03b62d44
Sebastian Guzdek <sebag93@gmail.com>
2020-09-25 22:09:06 +0200

    2.5 Kolejka testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/KolejkaTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaTests
+    {
+        [TestMethod]
+        public void UzyciePeek()
+        {
+            var kolejka = new Queue<int>();
+
+            kolejka.Enqueue(1);
+            kolejka.Enqueue(2);
+            kolejka.Enqueue(3);
+            kolejka.Enqueue(4);
+
+            Assert.AreEqual(1, kolejka.Peek());
+        }
+
+        [TestMethod]
+        public void UzycieContains()
+        {
+            var kolejka = new Queue<int>();
+
+            kolejka.Enqueue(1);
+            kolejka.Enqueue(2);
+            kolejka.Enqueue(3);
+            kolejka.Enqueue(4);
+
+            Assert.IsTrue(kolejka.Contains(3));
+        }
+
+        [TestMethod]
+        public void UzycieToArray()
+        {
+            var kolejka = new Queue<int>();
+
+            kolejka.Enqueue(1);
+            kolejka.Enqueue(2);
+            kolejka.Enqueue(3);
+            kolejka.Enqueue(4);
+
+            var tablica = kolejka.ToArray();
+            kolejka.Dequeue();
+
+            Assert.AreEqual(1, tablica[0]);
+            Assert.AreEqual(3, kolejka.Count);
+        }
+
+        [TestMethod]
+        public void UzycieClear()
+        {
+            var kolejka = new Queue<int>();
+
+            kolejka.Enqueue(1);
+            kolejka.Enqueue(2);
+            kolejka.Enqueue(3);
+            kolejka.Enqueue(4);
+
+            kolejka.Clear();
+
+            Assert.AreEqual(0, kolejka.Count);
+        }
+    }
+}
