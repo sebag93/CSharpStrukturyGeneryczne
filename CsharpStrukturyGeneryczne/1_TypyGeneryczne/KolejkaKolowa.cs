4aee229b7d511a167e4fc020b2a111d2752383fb
Sebastian Guzdek <sebag93@gmail.com>
2020-09-25 13:04:26 +0200

    1.3 Nasza kolekcja

---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/KolejkaKolowa.cs
@@ -0,0 +1,61 @@
+﻿namespace _1_TypyGeneryczne
+{
+    public class KolejkaKolowa
+    {
+        private double[] bufor;
+        private int poczatekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowa() : this(pojemnosc: 5)
+        {
+
+        }
+
+        public KolejkaKolowa(int pojemnosc)
+        {
+            bufor = new double[pojemnosc + 1];
+            poczatekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapisz(double wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == poczatekBufora)
+                poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+        }
+
+        public double Czytaj()
+        {
+            var wynik = bufor[poczatekBufora];
+            poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == poczatekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
+            }
+        }
+    }
+}
