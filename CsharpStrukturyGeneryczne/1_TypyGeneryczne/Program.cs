bf3067ee16843d187fc8030fe67e362911fe0a83
Sebastian Guzdek <sebag93@gmail.com>
2020-09-25 13:26:39 +0200

    1.4 KolejkaKolowa

--- CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1_TypyGeneryczne
 {
@@ -10,6 +6,26 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+
+            Console.WriteLine("W naszej kolejce jest: ");
+            while (!kolejka.JestPusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Czytaj());
+            } 
         }
     }
 }
