6333bd71627a43619fd34a42f4b611e4091b2a4a
Sebastian Guzdek <sebag93@gmail.com>
2020-09-25 21:37:38 +0200

    2.3 Lista testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1c17bbe-1416-4cb3-acbe-e04048f18095")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
