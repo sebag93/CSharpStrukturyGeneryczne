0d0bc59499d2f192dd602a7512f09b81f4642c6c
Sebastian Guzdek <sebag93@gmail.com>
2020-09-26 18:56:22 +0200

    2.13 Słownik testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/DictionaryTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class DictionaryTests
+    {
+        [TestMethod]
+        public void UzywamySlownikaJakoMapy()
+        {
+            var mapa = new Dictionary<int, string>();
+
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+
+            Assert.AreEqual("jeden", mapa[1]);
+        }
+
+        [TestMethod]
+        public void WyszukiwanieWSlowniku()
+        {
+            var mapa = new Dictionary<int, string>();
+
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+
+            Assert.IsTrue(mapa.ContainsKey(3));
+        }
+
+        [TestMethod]
+        public void UsuwanieZeSlownika()
+        {
+            var mapa = new Dictionary<int, string>();
+
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+
+            mapa.Remove(1);
+
+            Assert.AreEqual(2, mapa.Count);
+        }
+
+        [TestMethod]
+        public void CzyszczenieSlownika()
+        {
+            var mapa = new Dictionary<int, string>();
+
+            mapa.Add(1, "jeden");
+            mapa.Add(2, "dwa");
+            mapa.Add(3, "trzy");
+
+            mapa.Clear();
+
+            Assert.AreEqual(0, mapa.Count);
+        }
+
+        [TestMethod]
+        public void SlownikJakoSlownik()
+        {
+            var mapa = new Dictionary<string, string>();
+
+            mapa.Add("one", "jeden");
+            mapa.Add("two", "dwa");
+            mapa.Add("three", "trzy");
+
+            Assert.AreEqual("jeden", mapa["one"]);
+        }
+    }
+}
