04be733d06364577ac4da4037231fe267cf5197a
Sebastian Guzdek <sebag93@gmail.com>
2020-09-26 15:50:38 +0200

    2.8 HashSet testy

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/HashSetTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class HashSetTests
+    {
+        [TestMethod]
+        public void IntersectSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.IntersectWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 2, 3 }));
+        }
+
+        [TestMethod]
+        public void UnionSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.UnionWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void SymmetricExceptWithSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.SymmetricExceptWith(set2);
+
+            Assert.IsTrue(set1.SetEquals(new[] { 1, 4 }));
+        }
+
+        [TestMethod]
+        public void RemoveSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.Remove(1);
+
+            Assert.AreEqual(2, set1.Count);
+        }
+
+        [TestMethod]
+        public void RemoveWhereSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.RemoveWhere(x => x > 1);
+
+            Assert.AreEqual(1, set1.Count);
+        }
+
+        [TestMethod]
+        public void ContainsSets()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            Assert.IsTrue(set1.Contains(3));
+        }
+    }
+}
