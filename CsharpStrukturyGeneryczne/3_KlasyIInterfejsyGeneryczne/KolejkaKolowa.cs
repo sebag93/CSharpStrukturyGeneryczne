affa9fe19c06c3be49edaee4beaf0f23a8e047f3
Sebastian Guzdek <sebag93@gmail.com>
2020-09-27 15:31:03 +0200

    3 Ogólne klasy i interfejsy

---
+++ CsharpStrukturyGeneryczne/3_KlasyIInterfejsyGeneryczne/KolejkaKolowa.cs
@@ -0,0 +1,61 @@
+﻿namespace _3_KlasyIInterfejsyGeneryczne
+{
+    public class KolejkaKolowa<T>
+    {
+        private T[] bufor;
+        private int poczatekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowa() : this(pojemnosc: 5)
+        {
+
+        }
+
+        public KolejkaKolowa(int pojemnosc)
+        {
+            bufor = new T[pojemnosc + 1];
+            poczatekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapisz(T wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == poczatekBufora)
+                poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+        }
+
+        public T Czytaj()
+        {
+            var wynik = bufor[poczatekBufora];
+            poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == poczatekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
+            }
+        }
+    }
+}
---
+++ CsharpStrukturyGeneryczne/3_KlasyIInterfejsyGeneryczne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_KlasyIInterfejsyGeneryczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kolejka = new KolejkaKolowa<double>();
+            WprowadzanieDanych(kolejka);
+            PrzetwarzanieDanych(kolejka);
+        }
+
+        private static void PrzetwarzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            var suma = 0.0;
+            Console.WriteLine("W naszej kolejce jest: ");
+            while (!kolejka.JestPusty)
+            {
+                suma += kolejka.Czytaj();
+            }
+            Console.WriteLine(suma);
+        }
+
+        private static void WprowadzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
